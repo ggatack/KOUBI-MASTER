b3fe8c0404ee2fe34bb5b4f02c043607de4d4aba
ggatack <jrnymar907@gmail.com>
2026-08-26 13:46:03 +0200

    Remove next round event listener and scoring logic

--- script.js
+++ script.js
@@ -88,54 +88,5 @@ players.forEach((player)=>{
 
     `;
 
-});
-
-document.querySelector(".nextRound").addEventListener("click", ()=>{
-
-    let cards = document.querySelectorAll(".player-card");
-
-    cards.forEach((card,index)=>{
-
-        let request = Number(card.querySelector("select").value);
-        let done = Number(card.querySelector("input").value);
-
-        let points = 0;
-
-
-        // طلب صفر
-        if(request === 0){
-
-            if(done === 0){
-                points = 5;
-            }else{
-                points = -5;
-            }
-
-        }
-
-        // باقي الطلبات
-        else{
-
-            if(done >= request){
-                points = request;
-            }else{
-                points = -request;
-            }
-
-        }
-
-
-        game.scores[index] += points;
-
-    });
-
-
-    localStorage.setItem(
-        "koubiGame",
-        JSON.stringify(game)
-    );
-
-
-    alert("تم حساب الجولة ♠️");
 
 });
